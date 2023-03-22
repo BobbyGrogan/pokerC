448e1d66f40be121fac760ff0ce809f2e56903aa
BobbyGrogan <bobbydg11@gmail.com>
2023-03-22 10:28:27 -0400

    second

--- main.c
+++ main.c
@@ -3,20 +3,24 @@
 #include <string.h>
 
 int rank_to_num(const char *rank);
+char high_card(const char *two[2]);
+const char **make_deck();
 
 int main() {
-	// create deck of cards in order
-	const char *cards[52][2];
-	const char *ranks[13]; ranks[0] = "A"; ranks[1] = "2"; ranks[2] = "3"; ranks[3] = "4"; ranks[4] = "5"; ranks[5] = "6"; ranks[6] = "7"; ranks[7] = "8"; ranks[8] = "9"; ranks[9] = "10"; ranks[10] = "J"; ranks[11] = "Q"; ranks[12] = "K";
-	const char *suits[4]; suits[0] = "S"; suits[1] = "H"; suits[2] = "C"; suits[3] = "D";
-	int i = 0; int c; int a = 0;
-	while(i++ < 4) { c = -1; while(c++ < 12) { cards[a][0] = ranks[c]; cards[a][1] = suits[i]; a = a + 1; } }
-
-	printf("%s\n", cards[12][1]);
-	char j = rank_to_num(cards[12][0]);
-	printf("%i\n", j);
+	char **deck = make_deck();
+	printf("%s\n", deck[0]);
 
 	return 0;
+}
+
+const char **make_deck() {
+	const char *cards[52][2];
+        char *ranks[13]; ranks[0] = "A"; ranks[1] = "2"; ranks[2] = "3"; ranks[3] = "4"; ranks[4] = "5"; ranks[5] = "6"; ranks[6] = "7"; ranks[7] = "8"; ranks[8] = "9"; ranks[9] = "10"; ranks[10] = "J"; ranks[11] = "Q"; ranks[12] = "K";
+        char *suits[4]; suits[0] = "S"; suits[1] = "H"; suits[2] = "C"; suits[3] = "D";
+        int i = 0; int c; int a = 0;
+        while(i++ < 4) { c = -1; while(c++ < 12) { cards[a][0] = ranks[c]; cards[a][1] = suits[i]; a = a + 1; } }
+
+	return *cards;
 }
 
 int rank_to_num(const char *rank) {
@@ -27,4 +31,11 @@ int rank_to_num(const char *rank) {
         if(strcmp(rank, "J") == 0) { num = 11; return num; }
 	num = atoi(rank);
 	return num;
+}
+
+char high_card(const char *two[2]) {
+	int first = rank_to_num(two[0]);
+	int second = rank_to_num(two[1]);
+	if(first > second) { return *two[0]; }
+	return *two[1];
 }
